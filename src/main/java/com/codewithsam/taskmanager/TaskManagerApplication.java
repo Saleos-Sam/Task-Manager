d411a344a45a76ebedb1c4a320461a42de741159
Sampath-04 <sampathkaali002@gmail.com>
2025-07-31 23:11:33 +0530

    added: eureka client and docker file

--- src/main/java/com/codewithsam/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/codewithsam/taskmanager/TaskManagerApplication.java
@@ -2,8 +2,11 @@ package com.codewithsam.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
